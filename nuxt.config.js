b2b9afc062eda4b907c0c58c54eaa3abceeb1f9c
Antoine Boulanger <antoine@okayhq.com>
2022-03-21 15:53:37 -0700

    Add Analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,11 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-  buildModules: ['@nuxt/typescript-build'],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/google-analytics'],
   router: {
     base: '/handbook/'
+  },
+  googleAnalytics: {
+    id: process.env.GOOGLE_ANALYTICS_ID
   }
 })
