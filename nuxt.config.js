05753fef0e75c5f38b21b1a407667b683ff1662d
Antoine Boulanger <antoine@okayhq.com>
2023-05-23 10:48:34 -0700

    Remove analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,8 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-  buildModules: ['@nuxt/typescript-build', '@nuxtjs/google-analytics'],
-  router: {
-    base: '/handbook/'
-  },
-  i18n: {
-    baseUrl: '/handbook'
-  },
+  buildModules: ['@nuxt/typescript-build'],
   build: {
     transpile: [/^vue-github-button/]
-  },
-  env: {
-    eventsEndpoint: process.env.EVENTS_ENDPOINT  
-  },
-  googleAnalytics: {
-    id: process.env.GOOGLE_ANALYTICS_ID
   }
 })
