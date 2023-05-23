910124cd02c53f6e431daee666887de34fe24248
Antoine Boulanger <antoine@okayhq.com>
2023-05-23 11:02:20 -0700

    Correct baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ import theme from '@nuxt/content-theme-docs'
 
 export default theme({
   buildModules: ['@nuxt/typescript-build'],
+  app: {
+    baseURL: '/ee-handbook/'
+  },
   build: {
     transpile: [/^vue-github-button/]
   }
