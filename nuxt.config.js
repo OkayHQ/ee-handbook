e71a428778d6b3d8f22e61b4a8651afe879d454a
Antoine Boulanger <antoine@okayhq.com>
2023-05-24 10:03:27 -0700

    Firebase hosting

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,12 +2,6 @@ import theme from '@nuxt/content-theme-docs'
 
 export default theme({
   buildModules: ['@nuxt/typescript-build'],
-  app: {
-    baseURL: '/ee-handbook/'
-  },
-  router: {
-    base: '/ee-handbook/'
-  },
   build: {
     transpile: [/^vue-github-button/]
   }
